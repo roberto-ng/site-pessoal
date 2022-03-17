096bd2d1e21b36eb609115a21808b5f7d13c8949
Roberto Nazareth <nazarethguedesroberto@gmail.com>
2022-03-17 12:58:24 -0300

    .]

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -7,20 +7,6 @@ const Layout: FunctionComponent = ({ children }) => {
         <div>
             <Head>
                 <link rel="icon" href="/favicon.ico" />
-                <meta
-                name="description"
-                content="A cool blog"
-                />
-                {/*
-                <meta
-                property="og:image"
-                content={`https://og-image.now.sh/${encodeURI(
-                    siteTitle
-                )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
-                />
-                <meta name="og:title" content={siteTitle} />
-                <meta name="twitter:card" content="summary_large_image" />
-                */}
                 <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
             </Head>
 
