5a2498ee78cb356f93bca5b502adb95b357a6fb1
Roberto Nazareth <nazarethguedesroberto@gmail.com>
2022-03-19 12:26:53 -0300

    Logo

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -8,9 +8,14 @@ const Layout: FunctionComponent = ({ children }) => {
             <Head>
                 <link rel="icon" href="/favicon.ico" />
                 <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+                <link rel="preconnect" href="https://fonts.googleapis.com" />
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+                <link href="https://fonts.googleapis.com/css2?family=Suez+One&display=swap" rel="stylesheet" /> 
             </Head>
 
-            <header id={styles.header}></header>
+            <header id={styles.header}>
+                <h1>Roberto Guedes</h1>
+            </header>
 
             <main>{children}</main>
         </div>
