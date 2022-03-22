15ba81f2ca8f10df578c437604a88940bfd65f13
Roberto Nazareth <nazarethguedesroberto@gmail.com>
2022-03-22 14:00:51 -0300

    Usar preact

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+    reactStrictMode: true,
+    webpack: (config, { dev, isServer }) => {
+        if (!dev && !isServer) {
+            Object.assign(config.resolve.alias, {
+                react: 'preact/compat',
+                'react-dom/test-utils': 'preact/test-utils',
+                'react-dom': 'preact/compat',
+                'react/jsx-runtime': 'preact/jsx-runtime',
+            });
+        }
+
+        return config;
+  },
 }
 
 module.exports = nextConfig
