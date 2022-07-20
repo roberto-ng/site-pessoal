f2f9d31ca9745f7500e360aa37319b1562cd1d81
Roberto Nazareth <nazarethguedesroberto@gmail.com>
2022-07-19 22:32:49 -0300

    Adicionar analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,15 @@
 import { defineConfig } from 'astro/config';
 
+import partytown from "@astrojs/partytown";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    partytown({      
+      config: {
+        // Adds dataLayer.push as a forwarding-event.
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ],
+});
