52367e051d8988f29ce7858086ad4ac58fbef535
Roberto Nazareth <nazarethguedesroberto@gmail.com>
2022-07-27 11:38:54 -0300

    Adicionar path alias para a raiz do projeto, deixando os caminhos dos arquivos mais legiveis

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
-
 import partytown from "@astrojs/partytown";
+import { dirname, resolve } from 'path';
+import { fileURLToPath } from 'url';
+
+const rootDir = dirname(fileURLToPath(import.meta.url));
 
 // https://astro.build/config
 export default defineConfig({
@@ -12,4 +15,11 @@ export default defineConfig({
       },
     }),
   ],
+  vite: {
+    resolve: {
+      alias: {
+        '@': rootDir,
+      },
+    },
+  },
 });
