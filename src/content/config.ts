e20cdf11133cbcf1e30fae15d75f78618de17576
Roberto Nazareth <nazarethguedesroberto@gmail.com>
2023-02-21 12:54:43 -0300

    corrigir nome dos conteudos

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,31 +1,27 @@
 import { defineCollection, z } from 'astro:content';
 
-const postCollection = defineCollection({
-    schema: z.object({
-        title: z.string(),
-        date: z.coerce.date(),
-    }),
-});
-
-const projetoCollection = defineCollection({
-    schema: z.object({
-        titulo: z.string(),
-        repo: z.string().optional(),
-        download_link: z.string().optional(),
-        link: z.string().optional(),
-        descricao: z.string(),
-        thumbnail: z.string(),
-        tags: z.array(z.string()),
-        plataformas: z.array(z.string())
-    }),
-});
-
-const textoCollection = defineCollection({
-    schema: z.object({}),
-});
-
 export const collections = {
-    'post': postCollection,
-    'projeto': projetoCollection,
-    'texto': textoCollection,
+    'post': defineCollection({
+        schema: z.object({
+            title: z.string(),
+            date: z.coerce.date(),
+        }),
+    }),
+
+    'projeto': defineCollection({
+        schema: z.object({
+            titulo: z.string(),
+            repo: z.string().optional(),
+            download_link: z.string().optional(),
+            link: z.string().optional(),
+            descricao: z.string(),
+            thumbnail: z.string(),
+            tags: z.array(z.string()),
+            plataformas: z.array(z.string())
+        }),
+    }),
+
+    'texto': defineCollection({
+        schema: z.object({}),
+    }),
 };
