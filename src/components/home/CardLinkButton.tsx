6140dc9b998af61f274d93e5d4f9a5838b9cc3bb
Roberto <robertoguedesn@gmail.com>
2023-04-03 13:17:28 -0300

    ajuste de layout

--- src/components/home/CardLinkButton.tsx
+++ src/components/home/CardLinkButton.tsx
@@ -10,7 +10,7 @@ export const CardLinkButton: Component<Props> = (props) => {
         <div class="flex justify-center m-1">
             <a
                 href={props.href}
-                class="w-24 p-2 text-white no-underline transition-all bg-gray-800 rounded hover:bg-gray-500 dark:bg-zinc-700 dark:hover:bg-zinc-500"
+                class="w-28 p-2 text-white no-underline transition-all bg-gray-800 rounded hover:bg-gray-500 dark:bg-zinc-700 dark:hover:bg-zinc-500"
                 target="_blank"
                 rel="noreferrer"
             >
