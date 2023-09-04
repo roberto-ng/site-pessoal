7d7865984e3c753fd46ae9830209129d1ace7f64
Roberto Nazareth <nazarethguedesroberto@gmail.com>
2023-09-04 19:32:44 -0300

    usar bun

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,13 +4,12 @@ import { fileURLToPath } from 'url';
 import mdx from "@astrojs/mdx";
 import tailwind from "@astrojs/tailwind";
 import solidJs from "@astrojs/solid-js";
-import compress from "astro-compress";
 
 const rootDir = dirname(fileURLToPath(import.meta.url));
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), tailwind(), solidJs(), compress()],
+  integrations: [mdx(), tailwind(), solidJs()],
   vite: {
     resolve: {
       alias: {
