7b4db81bb790455a57c9bdfac26343760f15b92b
Roberto Nazareth <nazarethguedesroberto@gmail.com>
2025-01-05 01:18:05 -0300

    add: adicionar linhas animadas no fundo do site

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,22 @@
+import mdx from "@astrojs/mdx";
+import tailwind from "@astrojs/tailwind";
 import { defineConfig } from 'astro/config';
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
-import mdx from "@astrojs/mdx";
-import tailwind from "@astrojs/tailwind";
-import solidJs from "@astrojs/solid-js";
+
+import react from '@astrojs/react';
 
 const rootDir = dirname(fileURLToPath(import.meta.url));
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), tailwind(), solidJs()],
+  integrations: [
+    mdx(),
+    tailwind({ applyBaseStyles: false }),
+    react({
+      experimentalReactChildren: true,
+    }),
+  ],
   site: 'https://robertonazareth.dev/',
   output: 'static',
   vite: {
